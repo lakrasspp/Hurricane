868040739284918bda08f3b478e3dfc524316447
lakrass <src5.10.5@gmail.com>
2025-01-19 17:56:27 +0300

    flatted walls

--- src/haven/resutil/CaveTile.java
+++ src/haven/resutil/CaveTile.java
@@ -34,6 +34,7 @@ import haven.render.Pipe;
 
 public class CaveTile extends Tiler {
     public static final float h = 16;
+	public static Coord3f tilesz = Coord3f.of((float)MCache.tilesz.x,(float)MCache.tilesz.y, 0.0F);
     public final Material wtex;
     public final Tiler ground;
 	public static final Map<Material, Tex> tiles = new HashMap<>();
@@ -43,6 +44,7 @@ public class CaveTile extends Tiler {
 	public final Scan cs;
 	public final Vertex[][] wv;
 	private MapMesh.MapSurface ms;
+	private static float py = 0.27272728F;
 
 	public Walls(MapMesh m) {
 	    this.m = m;
@@ -53,14 +55,13 @@ public class CaveTile extends Tiler {
 
 	public Vertex[] fortile(Coord tc) {
 	    if(wv[cs.o(tc)] == null) {
-		if (OptWnd.flatCaveWallsCheckBox.a) {
-			Vertex[] buf = wv[cs.o(tc)] = new Vertex[4];
-			buf[0] = ms.fortile(tc);
-			buf[1] = ms.new Vertex(buf[0].x, buf[0].y, buf[0].z + 5f);
-			buf[2] = ms.new Vertex(buf[0].x + 0.01f, buf[0].y + 0.01f, buf[0].z + 5f);
-			buf[3] = ms.new Vertex(buf[0].x, buf[0].y, buf[0].z);
-
-		} else {
+//		if (OptWnd.flatCaveWallsCheckBox.a) {
+//			Vertex[] buf = wv[cs.o(tc)] = new Vertex[4];
+//			buf[0] = ms.fortile(tc);
+//			buf[1] = ms.new Vertex(buf[0].x, buf[0].y, buf[0].z + 5f);
+//			buf[2] = ms.new Vertex(buf[0].x + 1, buf[0].y + 1, buf[0].z + 5f);
+//			buf[3] = ms.new Vertex(buf[0].x, buf[0].y, buf[0].z);
+//		} else {
 		Random rnd = m.grnd(tc.add(m.ul));
 		Vertex[] buf = wv[cs.o(tc)] = new Vertex[4];
 		buf[0] = ms.new Vertex(ms.fortile(tc));
@@ -70,9 +71,24 @@ public class CaveTile extends Tiler {
 		    buf[i].y += (rnd.nextFloat() - 0.5f) * 3.0f;
 		    buf[i].z += (rnd.nextFloat() - 0.5f) * 3.5f;
 		}
-	    }
+//	    }
 		}
 	    return(wv[cs.o(tc)]);
+	}
+
+	public Vertex[] fortileFlat(Coord tc, float mul) {
+		if(wv[cs.o(tc)] == null) {
+			Vertex[] buf = wv[cs.o(tc)] = new Vertex[8];
+			buf[0] = ms.new Vertex(ms.fortile(tc).add(0.0F, 0.0F, 3.0F * mul));
+			buf[1] = ms.new Vertex(buf[0].add(0.0F, -CaveTile.tilesz.y, 0.0F));
+			buf[2] = ms.new Vertex(buf[0].add(CaveTile.tilesz.x, -CaveTile.tilesz.y, 0.0F));
+			buf[3] = ms.new Vertex(buf[0].add(CaveTile.tilesz.x, 0.0F, 0.0F));
+			buf[4] = ms.new Vertex(buf[0].add(0.0F, 0.0F, -3.0F));
+			buf[5] = ms.new Vertex(buf[4].add(0.0F, -CaveTile.tilesz.y, 0.0F));
+			buf[6] = ms.new Vertex(buf[4].add(CaveTile.tilesz.x, -CaveTile.tilesz.y, 0.0F));
+			buf[7] = ms.new Vertex(buf[4].add(CaveTile.tilesz.x, 0.0F, 0.0F));
+		}
+		return(wv[cs.o(tc)]);
 	}
     }
     public static final MapMesh.DataID<Walls> walls = MapMesh.makeid(Walls.class);
@@ -104,17 +120,45 @@ public class CaveTile extends Tiler {
 	}
     }
 
+	private void modelwallFlat(Walls w, Coord tc, boolean[] walled) {
+		Vertex[] lw = w.fortileFlat(tc, 1.0F);
+		MapMesh.MapSurface varW = w.ms;
+		Objects.requireNonNull(varW);
+		varW.new Face(lw[0], lw[1], lw[2]);
+		varW.new Face(lw[0], lw[2], lw[3]);
+	}
+
     public void model(MapMesh m, Random rnd, Coord lc, Coord gc) {
 	super.model(m, rnd, lc, gc);
 	Walls w = null;
-	for(int i = 0; i < 4; i++) {
+	boolean[] walled = new boolean[4];
+
+	for(int i = 0; i < 4; ++i) {
 	    int cid = m.map.gettile(gc.add(tces[i]));
-	    if(cid <= id || (m.map.tiler(cid) instanceof CaveTile))
-		continue;
-	    if(w == null) w = m.data(walls);
-	    modelwall(w, lc.add(tccs[(i + 1) % 4]), lc.add(tccs[i]));
+		if(OptWnd.flatCaveWallsCheckBox.a) {
+	    	if(cid > id || !(m.map.tiler(cid) instanceof CaveTile)) {
+				if(w == null) {
+					w = m.data(walls);
+				}
+				walled[i] = true;
+			}
+
+			if(walled[0] || walled[1] || walled[2] || walled[3]) {
+				modelwallFlat(w, lc, walled);
+			}
+	} else {
+			if(cid <= id || (m.map.tiler(cid) instanceof CaveTile))
+				continue;
+			if(w == null) w = m.data(walls);
+			modelwall(w, lc.add(tccs[(i + 1) % 4]), lc.add(tccs[i]));
+		}
 	}
+
     }
+
+	private Pipe.Op mkWtex() {
+		return this.wtex;
+	}
 
     private void mkwall(MapMesh m, Walls w, Coord ltc, Coord rtc) {
 	Vertex[] lw = w.fortile(ltc), rw = w.fortile(rtc);
@@ -124,9 +168,9 @@ public class CaveTile extends Tiler {
 	for(int i = 0; i < lv.length; i++) {
 	    float ty = (float)i / (float)(lv.length - 1);
 	    lv[i] = new Surface.MeshVertex(mod, lw[i]);
-	    tex.set(lv[i], new Coord3f(0, ty, 0));
+	    tex.set(lv[i], new Coord3f(0, ty, 2));
 	    rv[i] = new Surface.MeshVertex(mod, rw[i]);
-	    tex.set(rv[i], new Coord3f(1, ty, 0));
+	    tex.set(rv[i], new Coord3f(1, ty, 2));
 	}
 	for(int i = 0; i < lv.length - 1; i++) {
 	    mod.new Face(lv[i + 1], lv[i], rv[i + 1]);
@@ -134,15 +178,90 @@ public class CaveTile extends Tiler {
 	}
     }
 
+	private void mkwallFlat(MapMesh m, Walls w, Coord tc) {
+		Surface.Vertex[] lw = w.fortileFlat(tc, 1.0F);
+		MapMesh.Model mod = MapMesh.Model.get(m, mkWtex());
+		MeshBuf.Vertex[] lv = new MeshBuf.Vertex[20];
+		MeshBuf.Tex tex = mod.layer(mod.tex);
+		int i = 0;
+		lv[i] = new Surface.MeshVertex(mod, lw[0]);
+		tex.set(lv[i++], new Coord3f(0.0F, 1.0F, 0.0F));
+		lv[i] = new Surface.MeshVertex(mod, lw[1]);
+		tex.set(lv[i++], new Coord3f(0.0F, 0.0F, 0.0F));
+		lv[i] = new Surface.MeshVertex(mod, lw[2]);
+		tex.set(lv[i++], new Coord3f(1.0F, 0.0F, 0.0F));
+		lv[i] = new Surface.MeshVertex(mod, lw[3]);
+		tex.set(lv[i++], new Coord3f(1.0F, 1.0F, 0.0F));
+		lv[i] = new Surface.MeshVertex(mod, lw[3]);
+		tex.set(lv[i++], new Coord3f(0.0F, Walls.py, 0.0F));
+		lv[i] = new Surface.MeshVertex(mod, lw[7]);
+		tex.set(lv[i++], new Coord3f(0.0F, 0.0F, 0.0F));
+		lv[i] = new Surface.MeshVertex(mod, lw[4]);
+		tex.set(lv[i++], new Coord3f(1.0F, 0.0F, 0.0F));
+		lv[i] = new Surface.MeshVertex(mod, lw[0]);
+		tex.set(lv[i++], new Coord3f(1.0F, Walls.py, 0.0F));
+		lv[i] = new Surface.MeshVertex(mod, lw[2]);
+		tex.set(lv[i++], new Coord3f(0.0F, Walls.py, 0.0F));
+		lv[i] = new Surface.MeshVertex(mod, lw[6]);
+		tex.set(lv[i++], new Coord3f(0.0F, 0.0F, 0.0F));
+		lv[i] = new Surface.MeshVertex(mod, lw[7]);
+		tex.set(lv[i++], new Coord3f(1.0F, 0.0F, 0.0F));
+		lv[i] = new Surface.MeshVertex(mod, lw[3]);
+		tex.set(lv[i++], new Coord3f(1.0F, Walls.py, 0.0F));
+		lv[i] = new Surface.MeshVertex(mod, lw[1]);
+		tex.set(lv[i++], new Coord3f(0.0F, Walls.py, 0.0F));
+		lv[i] = new Surface.MeshVertex(mod, lw[5]);
+		tex.set(lv[i++], new Coord3f(0.0F, 0.0F, 0.0F));
+		lv[i] = new Surface.MeshVertex(mod, lw[6]);
+		tex.set(lv[i++], new Coord3f(1.0F, 0.0F, 0.0F));
+		lv[i] = new Surface.MeshVertex(mod, lw[2]);
+		tex.set(lv[i++], new Coord3f(1.0F, Walls.py, 0.0F));
+		lv[i] = new Surface.MeshVertex(mod, lw[0]);
+		tex.set(lv[i++], new Coord3f(0.0F, Walls.py, 0.0F));
+		lv[i] = new Surface.MeshVertex(mod, lw[4]);
+		tex.set(lv[i++], new Coord3f(0.0F, 0.0F, 0.0F));
+		lv[i] = new Surface.MeshVertex(mod, lw[5]);
+		tex.set(lv[i++], new Coord3f(1.0F, 0.0F, 0.0F));
+		lv[i] = new Surface.MeshVertex(mod, lw[1]);
+		tex.set(lv[i++], new Coord3f(1.0F, Walls.py, 0.0F));
+
+		for(int j = 0; j < 20; j += 4) {
+			Objects.requireNonNull(mod);
+			Objects.requireNonNull(mod);
+			mod.new Face( lv[j], lv[j + 1], lv[j + 2]);
+			Objects.requireNonNull(mod);
+			Objects.requireNonNull(mod);
+			mod.new Face( lv[j], lv[j + 2], lv[j + 3]);
+		}
+
+	}
+
     public void lay(MapMesh m, Random rnd, Coord lc, Coord gc) {
 	Walls w = null;
-	for(int i = 0; i < 4; i++) {
-	    int cid = m.map.gettile(gc.add(tces[i]));
-	    if(cid <= id || (m.map.tiler(cid) instanceof CaveTile))
-		continue;
-	    if(w == null) w = m.data(walls);
-	    mkwall(m, w, lc.add(tccs[(i + 1) % 4]), lc.add(tccs[i]));
+	boolean[] walled = new boolean[4];
+
+	for(int i = 0; i < 4; ++i) {
+		int cid = m.map.gettile(gc.add(tces[i]));
+		if (OptWnd.flatCaveWallsCheckBox.a) {
+			if (cid > id || !(m.map.tiler(cid) instanceof CaveTile)) {
+				if (w == null) {
+					w = m.data(walls);
+				}
+				walled[i] = true;
+			}
+
+		}
+		if (walled[0] || walled[1] || walled[2] || walled[3]) {
+			this.mkwallFlat(m, w, lc);
+		} else {
+			if(cid <= id || (m.map.tiler(cid) instanceof CaveTile))
+				continue;
+			if(w == null) w = m.data(walls);
+			mkwall(m, w, lc.add(tccs[(i + 1) % 4]), lc.add(tccs[i]));
+		}
 	}
+
+
 	if(ground != null) {
 		Tex tex = tiles.get(wtex);
 		if (tex == null && OptWnd.flatCaveWallsCheckBox.a) {
