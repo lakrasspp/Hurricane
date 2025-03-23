a108f1604733c3a6644675a0f97093d5e6f46da7
Nightdawg <unqdawg@gmail.com>
2025-03-23 14:18:02 +0200

    Make Giftwrap Cap 500% cooler

--- src/haven/AnimSprite.java
+++ src/haven/AnimSprite.java
@@ -81,6 +81,8 @@ public class AnimSprite extends Sprite {
 	if (this.res.name.equals("gfx/terobjs/items/hats/hipstersskullcap")) {
 		slot.cstate(Pipe.Op.compose(Location.rot(new Coord3f(0, 0, 1), 3.15f), Location.rot(new Coord3f(0, 1, 0), 0.2f),
 				Location.scale(1.2f, 1.1f, 1.1f), Location.xlate(new Coord3f(0, 0, -0.1f))));
+	} else if (this.res.name.equals("gfx/terobjs/items/hats/giftwrapcap")) {
+		slot.cstate(Pipe.Op.compose(Location.rot(new Coord3f(0, 0, 1), 3.15f)));
 	}
 	for(RenderTree.Node p : parts)
 	    slot.add(p);
