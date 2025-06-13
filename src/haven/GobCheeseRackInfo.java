23f5224bc78fd97b855f29ee0726592cb3f1b703
Nightdawg <unqdawg@gmail.com>
2025-06-13 05:04:57 +0300

    Clean up code for GobCheeseRackInfo and use static tex caching (improves fps a bit)

--- src/haven/GobCheeseRackInfo.java
+++ src/haven/GobCheeseRackInfo.java
@@ -7,13 +7,7 @@ import java.util.Map;
 
 public class GobCheeseRackInfo extends GobInfo {
 
-	private static final Color TEXT_COL = new Color(255, 255, 255, 255);
-	private static final Color BG = new Color(0, 0, 0, 84);
-	public static final int MARGIN = UI.scale(3);
-	public static final int PAD = 0;
-	private Tex infoTex;
-	private static final Map<Pair<Color, String>, Text.Line> TEXT_CACHE = new HashMap<>();
-
+	private static final Map<String, Tex> stageTexCache = new HashMap<>();
 
     protected GobCheeseRackInfo(Gob owner) {
 	super(owner);
@@ -26,15 +20,12 @@ public class GobCheeseRackInfo extends GobInfo {
 
 	@Override
 	protected Tex render() {
-		if(gob == null || gob.getres() == null) {return null;}
-		BufferedImage content = content();
-		if(content == null) {
-			infoTex = null;
-			return null;
-		}
-		if (infoTex != null)
-			return infoTex;
-		return infoTex = new TexI(ItemInfo.catimgsh(3, 0, BG, content));
+		return null;
+	}
+
+	@Override
+	public void ctick(double dt) {
+		content();
 	}
 
 	@Override
@@ -42,7 +33,7 @@ public class GobCheeseRackInfo extends GobInfo {
 	super.dispose();
     }
 
-	private BufferedImage content() {
+	private void content() {
 		for (Gob.Overlay ol : gob.ols) {
 			if (!ol.getSprResName().startsWith("gfx/fx/eq")) {continue;}
 			Sprite spr = Reflect.getFieldValue(ol.spr, "espr", Sprite.class);
@@ -56,26 +47,20 @@ public class GobCheeseRackInfo extends GobInfo {
 					text = "T" + name.substring(name.lastIndexOf("-") + 2);
 			}
 			if(text != null) {
-				spr.setTex2d(combine(PUtils.strokeImg(text(text, TEXT_COL).img)));
+				if (!stageTexCache.containsKey(text)) {
+					Tex stageTex = combine(PUtils.strokeImg(Text.std.renderstroked(text, Color.white, Color.black).img));
+					stageTexCache.put(text, stageTex);
+					spr.setTex2d(stageTex);
+				} else {
+					spr.setTex2d(stageTexCache.get(text));
+				}
 			} else {
 				spr.setTex2d(null);
 			}
 		}
-		return null;
-	}
-
-	private static Text.Line text(String text, Color col) {
-		Pair<Color, String> key = new Pair<>(col, text);
-		if(TEXT_CACHE.containsKey(key)) {
-			return TEXT_CACHE.get(key);
-		} else {
-			Text.Line line = Text.std.renderstroked(text, col, Color.black);
-			TEXT_CACHE.put(key, line);
-			return line;
-		}
 	}
 
 	private static Tex combine(BufferedImage... parts) {
-		return new TexI(ItemInfo.catimgsh(MARGIN, PAD, BG, parts));
+		return new TexI(ItemInfo.catimgsh(UI.scale(3), 0, null, parts));
 	}
 }
