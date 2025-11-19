0ae8508f946efeab0766f1fe18aea506c924ea01
Nightdawg <unqdawg@gmail.com>
2025-11-19 08:51:54 +0200

    Fix RidgeEdge after update

--- src/haven/res/gfx/tiles/flavor/ridge_edge/RidgeEdge.java
+++ src/haven/res/gfx/tiles/flavor/ridge_edge/RidgeEdge.java
@@ -12,7 +12,7 @@ import haven.resutil.Ridges.*;
 @haven.FromResource(name = "gfx/tiles/flavor/ridge-edge", version = 2)
 public class RidgeEdge implements Tileset.Flavor {
     public NodeWrap mat;
-    public float r = 0.5f, zoff = -0.0f, xoff = 0.0f;
+    public float r = 1.25f, zoff = -0.12f, xoff = -0.12f;
 
     public RidgeEdge(Resource sres, Object[][] argsv) {
 	for(Object[] args : argsv) {
@@ -127,7 +127,7 @@ public class RidgeEdge implements Tileset.Flavor {
     }
 
     public void flavor2(Buffer buf, Terrain trn, Random seed) {
-	float oz = (float)trn.map.getfz(trn.area.ul);
+	float oz = OptWnd.flatWorldCheckBox.a ? 0 : (float)trn.map.getfz(trn.area.ul);
 	Map<Coord3f, Point> points = new IdentityHashMap<>();
 	for(Coord tc : trn.tiles()) {
 	    RPart desc = getdesc(trn, tc);
