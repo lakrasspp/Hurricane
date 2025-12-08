43166dbce0911c90ad339273cec0d1c8c4d16c69
Nightdawg <unqdawg@gmail.com>
2025-12-08 22:18:58 +0200

    Add fetched resource code for ui/tt/slots-alt

---
+++ src/haven/res/ui/tt/slots_alt/Fac.java
@@ -0,0 +1,37 @@
+/* Preprocessed source code */
+package haven.res.ui.tt.slots_alt;
+
+import haven.*;
+import static haven.PUtils.*;
+import java.awt.image.*;
+import java.awt.Graphics;
+import java.awt.Font;
+import java.awt.Color;
+import java.util.*;
+
+/* >tt: Fac */
+@haven.FromResource(name = "ui/tt/slots-alt", version = 2)
+public class Fac implements ItemInfo.InfoFactory {
+    public ItemInfo build(ItemInfo.Owner owner, ItemInfo.Raw rawi, Object... args) {
+	Resource.Resolver rr = owner.context(Resource.Resolver.class);
+	int a = 1;
+	double pmin = ((Number)args[a++]).doubleValue();
+	double pmax = ((Number)args[a++]).doubleValue();
+	List<Resource> attrs = new LinkedList<Resource>();
+	while(args[a] != null)
+	    attrs.add(rr.getres((Integer)args[a++]).get());
+	a++;
+	int uses = (Integer)args[a++];
+	int used = (Integer)args[a++];
+	ISlots ret = new ISlots(owner, uses, used, pmin, pmax, attrs.toArray(new Resource[0]));
+	while(a < args.length) {
+	    Indir<Resource> res = rr.getres((Integer)args[a++]);
+	    Message sdt = Message.nil;
+	    if(args[a] instanceof byte[])
+		sdt = new MessageBuf((byte[])args[a++]);
+	    Object[] raw = (Object[])args[a++];
+	    ret.s.add(ret.new SItem(new ResData(res, sdt), raw));
+	}
+	return(ret);
+    }
+}
